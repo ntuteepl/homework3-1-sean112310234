9d4a97ed6dd44e8bba24ae701dd06844a89c84dd
黃翔 <146065123+sean112310234@users.noreply.github.com>
2023-10-22 19:48:15 -0500

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -2,7 +2,7 @@ int main() {
     int s1, d1, s2, d2, s3, d3;
     scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
     
-    int c = 1;  
+    int c ;  
     
 
     if ((s2>d1)||(s2>d3)||(s3>d1)||(s3>d2)||(s1>d2)||(s1>d3
@@ -14,6 +14,7 @@ int main() {
     if( (d1>s2&&d2>s3&&d3>s1)||(d1>s3&&d2>s1&&d3>s2)) {
         c=3; 
     }
+    else {c=1}
     
     printf("%d", c);
     
