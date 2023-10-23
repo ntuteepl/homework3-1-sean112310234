d846f09913adc7c6f3ae9376b995f7d91a8d2bd0
黃翔 <146065123+sean112310234@users.noreply.github.com>
2023-10-22 22:01:53 -0500

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int sumOfDigits(int n) {
+    int s = 0;
+    while (n > 0) {
+        s += n % 10;
+        n /= 10;
+    }
+    return s;
+}
+
+int main() {
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    int s1 = sumOfDigits(n1);
+    int s2 = sumOfDigits(n2);
+
+    if (s1 < s2) {
+        printf("%d %d\n", n1, n2);
+    } else if (s1 > s2) {
+        printf("%d %d\n", n2, n1);
+    } else {
+        if (n1 < n2) {
+            printf("%d %d\n", n1, n2);
+        } else {
+            printf("%d %d\n", n2, n1);
+        }
+    }
+
+    
+}
